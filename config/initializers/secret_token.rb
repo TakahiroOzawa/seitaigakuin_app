55a781aa67fb3a9710cf3643ac136c44b5ee64ca
Takahiro Ozawa <t.ozawa.toto@gmail.com>
2014-11-01 12:37:01 +0900

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SeitaigakuinApp::Application.config.secret_key_base = '79e2dd1e433612bc35df6a05ae0399abccb350d3be65299176a3f20bb11be6fb3e890136e4864f47eddfc41951a703289bf1650004827eca443fa74c3047f805'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SeitaigakuinApp::Application.config.session_store :cookie_store, key: '_seitaigakuin_app_session'
